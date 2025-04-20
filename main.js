3b09154a78f8500efb25d14f28243740282fc90d
siddhant-bharti <sbharti51@gmail.com>
2025-04-20 19:06:49 -0400

    Trying to add arrows on new map

--- main.js
+++ main.js
@@ -47,7 +47,66 @@ function visualizeMigration(data) {
         .domain([0, maxMigrantCount])
         .range([1, 10]);
 
+    // Create a group for migration paths that will be updated on map movement
+    const migrationGroup = flowMapG.append("g").attr("class", "migration-paths");
+
+    svg.append("g")
+    .selectAll("path")
+    .enter().append("path")
+    .attr("d", d3.geoPath().projection(projection))
+    .attr("fill", "#ddd")
+    .attr("stroke", "#999");
+    svg.append("defs").append("marker")
+        .attr("id", "arrowhead")
+        .attr("viewBox", "0 0 10 10")
+        .attr("refX", 6)  
+        .attr("refY", 5)
+        .attr("markerWidth", 2)  
+        .attr("markerHeight", 2)
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M 0 0 L 10 5 L 0 10 Z") 
+        .attr("fill", "blue");
     
+    // Function to update path positions based on map view
+    function updatePaths() {
+        console.log("Called")
+        migrationGroup.selectAll("path").remove();
+
+        data.forEach(d => {
+            let origin = projection([+d.origin_lon, +d.origin_lat]);
+            let destination = projection([+d.dest_lon, +d.dest_lat]);
+            
+            if (!origin || !destination) return;
+            
+            let strokeWidth = thicknessScale(d.migrants);
+            let randomOffset = Math.random() * 100 - 50;
+            let controlPoint = [
+                (origin[0] + destination[0]) / 2,
+                Math.min(origin[1], destination[1]) + randomOffset
+            ];
+            
+            let line = d3.line()
+                .x(d => d[0])
+                .y(d => d[1])
+                .curve(d3.curveBasis);
+                
+            let points = [origin, controlPoint, destination];
+            
+            migrationGroup.append("path")
+                .attr("d", line(points))
+                .attr("fill", "none")
+                .attr("stroke", "blue")
+                .attr("stroke-width", strokeWidth)
+                .attr("opacity", 0.5)
+                .attr("marker-end", "url(#arrowhead)");
+        });
+    }
+    
+    updatePaths();
+    
+    myMap.on("moveend", updatePaths);
+    myMap.on("zoomend", updatePaths);
 }
 
 
