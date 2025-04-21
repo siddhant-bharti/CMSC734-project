6d4591c0daf87bbb44c1a7716af06eef6064aecc
Shrey <2019cs10400@gmail.com>
2025-04-20 21:06:09 -0400

    Replaced GeoJSON with Leaflet

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-import {loadDataSet, createCountryISOMapping} from "./data-utils.js";
-
-
 var map = d3.select('#map');
 var mapWidth = +map.attr('width');
 var mapHeight = +map.attr('height');
+var vertices = d3.map();
+var activeMapType = 'nodes_links';
+var nodeFeatures = [];
 
 var myMap = L.map('map').setView([0, 0], 2);
-
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {
     maxZoom: 10,
@@ -17,147 +15,44 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 
 var svgLayer = L.svg();
 svgLayer.addTo(myMap)
-
-
 var svg = d3.select('#map').select('svg');
-var width = +svg.attr("width"), height = +svg.attr("height");
-var flowMapG = svg.select('g').attr('class', 'leaflet-zoom-hide');
-
-// Projection from lat, long to x, y
-const projection = d3.geoMercator()
-    .scale(150)
-    .translate([width / 2, height / 1.5]);
+var nodeLinkG = svg.select('g')
+.attr('class', 'leaflet-zoom-hide');
 
 Promise.all([
-    // loadDataSet("./datasets/dataset_denormalized_enriched_pruned.csv")
-    loadDataSet("./datasets/filtered_origin_RWA.csv")
+    d3.csv('./datasets/test.csv', function(row) {
+        var link = {origin: [+row['origin_lat'], +row['origin_lon']], destination: [+row['dest_lat'], +row['dest_lon']], 
+            migrantCount: +row['migrants'] };
+        return link; 
+    })     
 ]).then(function(data) {
-    var migrationData = data[0];
-    // const {countryToIso, isoToCountry} = createCountryISOMapping(migrationData);
-    // console.log(countryToIso);
-    // console.log(isoToCountry);
-    visualizeMigration(migrationData);
+    var links = data[0];
+    drawFlowMap(links);
 });
 
-function visualizeMigration(data) {
-    // console.log(data);
-    data.forEach(d => d.migrants = +d.migrants);
-    const maxMigrantCount = d3.max(data, d => d.migrants);
-    const thicknessScale = d3.scaleLinear()
-        .domain([0, maxMigrantCount])
-        .range([1, 10]);
+function drawFlowMap(links) {
+    const maxMigrantCount = d3.max(links, d => d.migrantCount);
+    const thicknessScale = d3.scaleLinear().domain([0, maxMigrantCount]).range([1, 10]);
 
-    // Create a group for migration paths that will be updated on map movement
-    const migrationGroup = flowMapG.append("g").attr("class", "migration-paths");
+    nodeLinkG.selectAll('.grid-link')
+    .data(links)
+    .enter().append('line')
+    .attr('class', 'grid-link')
+    .style('stroke', '#999')
+    .style('stroke-opacity', 0.5)
+    .style('stroke-width', function(d){ return thicknessScale(d.migrantCount) })      
+    myMap.on('zoomend', updateLayers);
+    updateLayers();
+}
 
-    svg.append("g")
-    .selectAll("path")
-    .enter().append("path")
-    .attr("d", d3.geoPath().projection(projection))
-    .attr("fill", "#ddd")
-    .attr("stroke", "#999");
-    svg.append("defs").append("marker")
-        .attr("id", "arrowhead")
-        .attr("viewBox", "0 0 10 10")
-        .attr("refX", 6)  
-        .attr("refY", 5)
-        .attr("markerWidth", 2)  
-        .attr("markerHeight", 2)
-        .attr("orient", "auto")
-        .append("path")
-        .attr("d", "M 0 0 L 10 5 L 0 10 Z") 
-        .attr("fill", "blue");
-    
-    // Function to update path positions based on map view
-    function updatePaths() {
-        console.log("Called")
-        migrationGroup.selectAll("path").remove();
-
-        data.forEach(d => {
-            let origin = projection([+d.origin_lon, +d.origin_lat]);
-            let destination = projection([+d.dest_lon, +d.dest_lat]);
-            
-            if (!origin || !destination) return;
-            
-            let strokeWidth = thicknessScale(d.migrants);
-            let randomOffset = Math.random() * 100 - 50;
-            let controlPoint = [
-                (origin[0] + destination[0]) / 2,
-                Math.min(origin[1], destination[1]) + randomOffset
-            ];
-            
-            let line = d3.line()
-                .x(d => d[0])
-                .y(d => d[1])
-                .curve(d3.curveBasis);
-                
-            let points = [origin, controlPoint, destination];
-            
-            migrationGroup.append("path")
-                .attr("d", line(points))
-                .attr("fill", "none")
-                .attr("stroke", "blue")
-                .attr("stroke-width", strokeWidth)
-                .attr("opacity", 0.5)
-                .attr("marker-end", "url(#arrowhead)");
-        });
-    }
-    
-    updatePaths();
-    
-    myMap.on("moveend", updatePaths);
-    myMap.on("zoomend", updatePaths);
+function updateLayers(){
+    nodeLinkG.selectAll('.grid-link')
+    .attr('x1', function(d){return myMap.latLngToLayerPoint(d.origin).x})
+    .attr('y1', function(d){return myMap.latLngToLayerPoint(d.origin).y})
+    .attr('x2', function(d){return myMap.latLngToLayerPoint(d.destination).x})
+    .attr('y2', function(d){return myMap.latLngToLayerPoint(d.destination).y});
 }
 
 
 
-// d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(worldData => {
-//     svg.append("g")
-//         .selectAll("path")
-//         .data(worldData.features)
-//         .enter().append("path")
-//         .attr("d", d3.geoPath().projection(projection))
-//         .attr("fill", "#ddd")
-//         .attr("stroke", "#999");
-//     d3.csv("./datasets/filtered_origin_RWA.csv").then(data => {
-//         data.forEach(d => d.migrants = +d.migrants);
-//         const maxMigrantCount = d3.max(data, d => d.migrants);
-//         const thicknessScale = d3.scaleLinear()
-//             .domain([0, maxMigrantCount])
-//             .range([1, 10]);
-//         svg.append("defs").append("marker")
-//             .attr("id", "arrowhead")
-//             .attr("viewBox", "0 0 10 10")
-//             .attr("refX", 6)  
-//             .attr("refY", 5)
-//             .attr("markerWidth", 2)  
-//             .attr("markerHeight", 2)
-//             .attr("orient", "auto")
-//             .append("path")
-//             .attr("d", "M 0 0 L 10 5 L 0 10 Z") 
-//             .attr("fill", "blue");
-//         data.forEach(d => {
-//             let origin = projection([+d.origin_lon, +d.origin_lat]);
-//             let destination = projection([+d.dest_lon, +d.dest_lat]);
-//             if (!origin || !destination) return;
-//             let strokeWidth = thicknessScale(d.migrants);
-//             let randomOffset = Math.random() * 100 - 50;
-//             let controlPoint = [
-//                 (origin[0] + destination[0]) / 2,
-//                 Math.min(origin[1], destination[1]) + randomOffset
-//             ];
-//             let line = d3.line()
-//                 .x(d => d[0])
-//                 .y(d => d[1])
-//                 .curve(d3.curveBasis)
-//             let points = [origin, controlPoint, destination];
-//             svg.append("path")
-//                 .attr("d", line(points))
-//                 .attr("fill", "none")
-//                 .attr("stroke", "blue")
-//                 .attr("stroke-width", strokeWidth)
-//                 .attr("opacity", 0.5)
-//                 .attr("marker-end", "url(#arrowhead)"); 
-//         });
-//     }).catch(error => console.error("Error loading CSV:", error));
-// });
+
